b611fba877b8f716a94953effd495e6361bec057
Aniket Ladukar <ladukaraniket.projects@gmail.com>
2023-09-13 15:02:50 -0700

    added documentation and implemented a newer method of AA

--- Assignment-1/ImageDisplay.java
+++ Assignment-1/ImageDisplay.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.WindowConstants;
 
 class ImageParams {
 	private int imageWidth;
@@ -24,6 +25,13 @@ class ImageParams {
 
 	int imageScale;
 
+	/**
+	 * <pre>
+	 *Reads the file at the given Path and intializes the ImageParams Object
+	 * </pre>
+	 * 
+	 * @param imgPath
+	 */
 	public ImageParams(String imgPath) {
 
 		File file = new File(imgPath);
@@ -40,9 +48,7 @@ class ImageParams {
 			raf.close();
 
 			this.imageWidth = 1920 * imageScale;
-			System.out.println("imageWidth " + imageWidth);
 			this.imageHeight = 1080 * imageScale;
-			System.out.println("imageHeight " + imageHeight);
 
 			this.red = Arrays.copyOfRange(bytes, 0, frameLength / 3);
 			this.green = Arrays.copyOfRange(bytes, frameLength / 3, frameLength / 3 * 2);
@@ -84,9 +90,14 @@ class WindowParams {
 	int windowWidth;
 	int windowHeight;
 	int overlayDim;
+	int overlayOffset;
 	float scalingFactor;
 	float samplingFactor;
 	boolean antiAliasingEnabled;
+
+	public WindowParams() {
+
+	}
 
 	public WindowParams(int windowWidth, int windowHeight) {
 		this.windowWidth = windowWidth;
@@ -122,6 +133,11 @@ class WindowParams {
 
 	public void setOverlayDim(int overlayDim) {
 		this.overlayDim = overlayDim;
+		this.overlayOffset = overlayDim / 2;
+	}
+
+	public int getOverlayOffset() {
+		return overlayOffset;
 	}
 
 	public float getScalingFactor() {
@@ -156,41 +172,21 @@ public class ImageDisplay {
 	WindowParams window;
 	ImageParams image;
 
-	int offsetX;
-	int offsetY;
-	int newWidth;
-	int newHeight;
-
 	boolean ctrlKeyPressed = false;
 
 	/**
-	 * Read Image RGB
-	 * Reads the image of given windowWidth and windowHeight at the given imgPath
-	 * into the
-	 * provided BufferedImage.
+	 * <pre>
+	 * Paints the initial view of scaled Image
+	 * </pre>
 	 */
-	private void readScaledImageRGB() {
+	private void renderInitialImage() {
 		try {
-			// this.offsetX = (int) ((1.0 - 1.0 / samplingFactor) / 2.0 *
-			// this.window.getWindowWidth());
-			// this.offsetY = (int) ((1.0 - 1.0 / samplingFactor) / 2.0 *
-			// this.window.getWindowHeight());
-			this.newWidth = (int) (1.0 / this.window.getSamplingFactor() * this.window.getWindowWidth());
-			this.newHeight = (int) (1.0 / this.window.getSamplingFactor() * this.window.getWindowHeight());
-
-			System.out.println("samplingFactor " + this.window.getSamplingFactor());
-			System.out.println("offsetX " + offsetX);
-			System.out.println("offsetY " + offsetY);
-			System.out.println("newWidth " + newWidth);
-			System.out.println("newHeight " + newHeight);
-
 			// Render the image as per scale
-			renderDefault();
+			renderPartial(0, this.window.getWindowWidth(), 0, this.window.getWindowHeight());
 
 			// Apply Anti-Aliasing
 			if (this.window.antiAliasingEnabled) {
-				// applyAntiAliasing();
-				applyPartialAntiAliasing(0, this.newWidth, 0, this.newHeight);
+				applyPartialAntiAliasing(0, this.window.getWindowWidth(), 0, this.window.getWindowHeight());
 			}
 
 		} catch (Exception e) {
@@ -198,13 +194,25 @@ public class ImageDisplay {
 		}
 	}
 
-	private void renderDefault() {
-		int renderStartX = 0;
-		int renderStartY = 0;
+	/**
+	 * <pre>
+	 *Renders partial area of the canvas bound by the coordinates specified as inputs.
+	 *This is useful while repainting the area once the overlay moves to a different location
+	 * </pre>
+	 */
+	private void renderPartial(int renderStartX, int renderEndX, int renderStartY, int renderEndY) {
+
+		renderStartX = renderStartX > 8 ? renderStartX : 0;
+		renderStartY = renderStartY > 32 ? renderStartY : 0;
+		renderEndX = renderEndX < this.window.getWindowWidth() ? renderEndX : this.window.getWindowWidth();
+		renderEndY = renderEndY < this.window.getWindowHeight() ? renderEndY : this.window.getWindowHeight();
+
 		int ind;
-		for (float y = 0; y < this.image.getImageHeight(); y += this.window.getSamplingFactor()) {
+		for (float y = renderStartY * this.window.getSamplingFactor(); y < renderEndY
+				* this.window.getSamplingFactor(); y += this.window.getSamplingFactor()) {
 			int xcoord = renderStartX;
-			for (float x = 0; x < this.image.getImageWidth(); x += this.window.getSamplingFactor(), xcoord++) {
+			for (float x = renderStartX * this.window.getSamplingFactor(); x < renderEndX
+					* this.window.getSamplingFactor(); x += this.window.getSamplingFactor(), xcoord++) {
 
 				ind = (int) x + (int) y * this.image.getImageWidth();
 
@@ -226,66 +234,39 @@ public class ImageDisplay {
 		}
 	}
 
-	private void renderPartial(int renderStartX, int renderEndX, int renderStartY, int renderEndY) {
-		// renderStartX = 0;
-		// renderStartY = 0;
-
-		renderStartX = renderStartX > 8 ? renderStartX : 0;
-		renderStartY = renderStartY > 32 ? renderStartY : 0;
-		renderEndX = renderEndX < this.newWidth ? renderEndX : this.newWidth;
-		renderEndY = renderEndY < this.newHeight ? renderEndY : this.newHeight;
-
-		int ind;
-		for (float y = renderStartY * this.window.getSamplingFactor(); y < renderEndY
-				* this.window.getSamplingFactor(); y += this.window.getSamplingFactor()) {
-			int xcoord = renderStartX;
-			for (float x = renderStartX * this.window.getSamplingFactor(); x < renderEndX
-					* this.window.getSamplingFactor(); x += this.window.getSamplingFactor(), xcoord++) {
-
-				ind = (int) x + (int) y * this.window.getWindowWidth();
-
-				byte r = this.image.red[ind];
-				byte g = this.image.green[ind];
-				byte b = this.image.blue[ind];
-
-				int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
-
-				// TODO remove try catch
-				try {
-					this.imgOne.setRGB(xcoord, renderStartY, pix);
-
-				} catch (Exception e) {
-					break;
-				}
-			}
-			renderStartY++;
-		}
-	}
-
-	public void applyAntiAliasing() {
-		int AAGridSize = 1; // creates a W x W filter where W = 2*AAGridSize + 1
-		int pixVal;
-		int aaPixRed, aaPixGreen, aaPixBlue;
+	public void applyPartialAntiAliasing(int xStart, int xEnd, int yStart, int yEnd) {
+		int AAGridSize = 2; // creates a W x W filter where W = 2*AAGridSize + 1
+		long aaPixRed, aaPixGreen, aaPixBlue;
 		int count;
 
-		for (int row = 0; row < this.newWidth; row++) {
-			for (int col = 0; col < this.newHeight; col++) {
+		xStart = xStart > 8 ? xStart : 0;
+		yStart = yStart > 32 ? yStart : 0;
+		xEnd = xEnd < this.window.getWindowWidth() ? xEnd : this.window.getWindowWidth();
+		yEnd = yEnd < this.window.getWindowHeight() ? yEnd : this.window.getWindowHeight();
+
+		int ind;
+		for (float y = yStart * this.window.getSamplingFactor(); y < yEnd
+				* this.window.getSamplingFactor(); y += this.window.getSamplingFactor()) {
+			int xcoord = xStart;
+			for (float x = xStart * this.window.getSamplingFactor(); x < xEnd
+					* this.window.getSamplingFactor(); x += this.window.getSamplingFactor(), xcoord++) {
+
 				aaPixRed = 0;
 				aaPixGreen = 0;
 				aaPixBlue = 0;
 				count = 0;
 
-				for (int aaRow = row - AAGridSize; aaRow <= row + AAGridSize; aaRow++) {
-					for (int aaCol = col - AAGridSize; aaCol <= col + AAGridSize; aaCol++) {
+				for (int aaRow = (int) x - AAGridSize; aaRow <= x + AAGridSize; aaRow++) {
+					for (int aaCol = (int) y - AAGridSize; aaCol <= y + AAGridSize; aaCol++) {
 
-						if (aaRow >= 0 && aaRow < this.newWidth && aaCol >= 0
-								&& aaCol < this.newHeight) {
+						if (aaRow >= 0 && aaRow < this.image.getImageWidth() && aaCol >= 0
+								&& aaCol < this.image.getImageHeight()) {
 
-							pixVal = this.imgOne.getRGB(aaRow, aaCol);
+							ind = aaRow + aaCol * this.image.getImageWidth();
 
-							aaPixRed += (pixVal >> 16) & 0xff;
-							aaPixGreen += (pixVal >> 8) & 0xff;
-							aaPixBlue += pixVal & 0xff;
+							aaPixRed += this.image.red[ind] & 0xff;
+							aaPixGreen += this.image.green[ind] & 0xff;
+							aaPixBlue += this.image.blue[ind] & 0xff;
 
 							count++;
 						}
@@ -293,20 +274,33 @@ public class ImageDisplay {
 					}
 				}
 
-				int aaPix = 0xff000000 | ((aaPixRed / count & 0xff) << 16) | ((aaPixGreen / count & 0xff) << 8)
-						| (aaPixBlue / count & 0xff);
+				aaPixRed/=count;
+				aaPixGreen/=count;
+				aaPixBlue/=count;
+				int aaPix = 0xff000000 | (((int)aaPixRed & 0xff) << 16) | (((int)aaPixGreen  & 0xff) << 8)
+						| ((int)aaPixBlue & 0xff);
 
+
+				// TODO remove try catch
 				try {
-					this.imgOne.setRGB(row, col, aaPix);
+					this.imgOne.setRGB(xcoord, yStart, aaPix);
 
 				} catch (Exception e) {
 					break;
 				}
 			}
+			yStart++;
 		}
+
 	}
 
-	public void applyPartialAntiAliasing(int xStart, int xEnd, int yStart, int yEnd) {
+	/**
+	 * <pre>
+	 *Applies Anti-aliasing to partial area of the canvas bound by the coordinates specified as inputs.
+	 *This is useful while applying AA to area repainted by renderPartial() instead of reapplying AA to the entire canvas.
+	 * </pre>
+	 */
+	public void applyPartialAntiAliasingOld(int xStart, int xEnd, int yStart, int yEnd) {
 		int AAGridSize = 1; // creates a W x W filter where W = 2*AAGridSize + 1
 		int pixVal;
 		int aaPixRed, aaPixGreen, aaPixBlue;
@@ -314,8 +308,8 @@ public class ImageDisplay {
 
 		xStart = xStart > 8 ? xStart : 0;
 		yStart = yStart > 32 ? yStart : 0;
-		xEnd = xEnd < this.newWidth ? xEnd : this.newWidth;
-		yEnd = yEnd < this.newHeight ? yEnd : this.newHeight;
+		xEnd = xEnd < this.window.getWindowWidth() ? xEnd : this.window.getWindowWidth();
+		yEnd = yEnd < this.window.getWindowHeight() ? yEnd : this.window.getWindowHeight();
 
 		for (int row = xStart; row < xEnd; row++) {
 			for (int col = yStart; col < yEnd; col++) {
@@ -355,9 +349,18 @@ public class ImageDisplay {
 		}
 	}
 
+	/**
+	 * <pre>
+	 *Displays the overlay for a given position (x,y).
+	 *The Overlay displays the part of the Image at its Native resolution.
+	 * </pre>
+	 * 
+	 * @param x
+	 * @param y
+	 */
 	public void displayOverLay(int x, int y) {
 
-		int gridSize = this.window.getOverlayDim();
+		int gridOffset = this.window.getOverlayOffset();
 		int ind;
 
 		int pointerCoordX = x - 8;
@@ -368,20 +371,25 @@ public class ImageDisplay {
 		OGcoordX = (int) (pointerCoordX * this.window.getSamplingFactor());
 		OGcoordY = (int) (pointerCoordY * this.window.getSamplingFactor());
 
-		OGcoordX -= this.window.getOverlayDim();
+		OGcoordX -= gridOffset;
 
-		for (int boxRow = pointerCoordX - gridSize; boxRow < pointerCoordX + gridSize; boxRow++) {
-			for (int boxCol = pointerCoordY - gridSize,
-					yCoord = OGcoordY - this.window.getOverlayDim(); boxCol < pointerCoordY + gridSize; boxCol++) {
+		for (int boxRow = pointerCoordX - gridOffset; boxRow < pointerCoordX + gridOffset; boxRow++) {
+			for (int boxCol = pointerCoordY - gridOffset,
+					yCoord = OGcoordY - gridOffset; boxCol < pointerCoordY
+							+ gridOffset; boxCol++) {
 
-				if (boxRow >= 0 && boxRow < newWidth && boxCol >= 0
-						&& boxCol < newHeight && OGcoordX > 0 && OGcoordX < this.image.getImageWidth()
+				if (boxRow >= 0
+						&& boxRow < this.window.getWindowWidth() && boxCol >= 0
+						&& boxCol < this.window.getWindowHeight() && OGcoordX > 0
+						&& OGcoordX < this.image.getImageWidth()
 						&& yCoord > 0
 						&& yCoord < this.image.getImageHeight()) {
 
-					ind = (int) OGcoordX + (int) yCoord * this.image.getImageWidth();
+					ind = OGcoordX + yCoord * this.image.getImageWidth();
+
 					int boxPix = 0xff000000 | ((image.red[ind] & 0xff) << 16) | ((image.green[ind] & 0xff) << 8)
 							| (image.blue[ind] & 0xff);
+
 					this.imgOne.setRGB(boxRow, boxCol, boxPix);
 				}
 
@@ -395,17 +403,18 @@ public class ImageDisplay {
 
 		this.image = new ImageParams(args[0]);
 
-		this.window = new WindowParams(this.image.getImageWidth(), this.image.getImageHeight());
+		this.window = new WindowParams();
 		this.window.setScalingFactor(Float.parseFloat(args[1]));
 		this.window.setAntiAliasingEnabled(Integer.parseInt(args[2]) == 1);
 		this.window.setOverlayDim(Integer.parseInt(args[3]));
+		this.window.setWindowHeight((int) (this.image.getImageHeight() * this.window.getScalingFactor()));
+		this.window.setWindowWidth((int) (this.image.getImageWidth() * this.window.getScalingFactor()));
 
 		// Read in the specified image
-		this.imgOne = new BufferedImage((int) (this.window.getWindowWidth() * this.window.getScalingFactor()),
-				(int) (this.window.getWindowHeight() * this.window.getScalingFactor()),
+		this.imgOne = new BufferedImage(this.window.getWindowWidth(), this.window.getWindowHeight(),
 				BufferedImage.TYPE_INT_RGB);
 
-		readScaledImageRGB();
+		renderInitialImage();
 
 		// Use label to display the image
 		frame = new JFrame();
@@ -416,7 +425,6 @@ public class ImageDisplay {
 		frame.addKeyListener(new KeyListener() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				// TODO Auto-generated method stub
 				if (!ctrlKeyPressed && e.getKeyCode() == 17) {
 					ctrlKeyPressed = true;
 				}
@@ -424,14 +432,13 @@ public class ImageDisplay {
 
 			@Override
 			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
 				if (ctrlKeyPressed && e.getKeyCode() == 17) {
 					ctrlKeyPressed = false;
-					// renderDefault();
-					renderPartial(0, newWidth, 0, newHeight);
+
+					renderPartial(0, window.getWindowWidth(), 0, window.getWindowHeight());
 
 					if (window.antiAliasingEnabled) {
-						applyPartialAntiAliasing(0, newWidth, 0, newHeight);
+						applyPartialAntiAliasing(0, window.getWindowWidth(), 0, window.getWindowHeight());
 					}
 					frame.repaint();
 				}
@@ -439,7 +446,7 @@ public class ImageDisplay {
 
 			@Override
 			public void keyTyped(KeyEvent e) {
-
+				// No implementation required for this event
 			}
 		});
 		frame.addMouseMotionListener(new MouseMotionListener() {
@@ -449,22 +456,21 @@ public class ImageDisplay {
 
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				// TODO Auto-generated method stub
-
+				// No implementation required for this event
 			}
 
 			@Override
 			public void mouseMoved(MouseEvent e) {
 
-				renderPartial(prevX - window.getOverlayDim() - 50, prevX + window.getOverlayDim() + 50,
-						prevY - window.getOverlayDim() - 50, prevY + window.getOverlayDim() + 50);
-
-				if (window.antiAliasingEnabled) {
-					applyPartialAntiAliasing(prevX - window.getOverlayDim() - 50, prevX + window.getOverlayDim() + 50,
-							prevY - window.getOverlayDim() - 50, prevY + window.getOverlayDim() + 50);
-				}
-
 				if (ctrlKeyPressed) {
+					renderPartial(prevX - window.getOverlayOffset() - 50, prevX + window.getOverlayOffset() + 50,
+							prevY - window.getOverlayOffset() - 50, prevY + window.getOverlayOffset() + 50);
+
+					if (window.antiAliasingEnabled) {
+						applyPartialAntiAliasing(prevX - window.getOverlayOffset() - 50,
+								prevX + window.getOverlayOffset() + 50,
+								prevY - window.getOverlayOffset() - 50, prevY + window.getOverlayOffset() + 50);
+					}
 					displayOverLay(e.getX(), e.getY());
 				}
 
@@ -476,6 +482,22 @@ public class ImageDisplay {
 		});
 
 		frame.setResizable(false);
+
+		StringBuilder title = new StringBuilder();
+		String spacing = "      ";
+
+		title.append("S: " + this.window.getScalingFactor());
+		title.append(spacing);
+		title.append("AA: " + this.window.antiAliasingEnabled);
+		title.append(spacing);
+		title.append("overlay: " + this.window.getOverlayDim() + "x" + this.window.getOverlayDim());
+		title.append(spacing);
+		title.append("this.window.getWindowWidth(): " + this.window.getWindowWidth());
+		title.append(spacing);
+		title.append("this.window.getWindowHeight(): " + this.window.getWindowHeight());
+
+		frame.setTitle(title.toString());
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
